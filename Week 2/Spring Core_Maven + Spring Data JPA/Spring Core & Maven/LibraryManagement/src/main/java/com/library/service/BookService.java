50550394762f1e2a87a4ad88c91ae686aad3162e
Saumil20042004 <151654941+Saumil20042004@users.noreply.github.com>
2026-07-02 22:11:06 +0530

    added spring exercises 1, 2, 4 in week 2

---
+++ Week 2/Spring Core_Maven + Spring Data JPA/Spring Core & Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,19 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void manageBooks() {
+        System.out.println("Managing books in service...");
+        if (bookRepository != null) {
+            bookRepository.fetchBooks();
+        }
+    }
+}
